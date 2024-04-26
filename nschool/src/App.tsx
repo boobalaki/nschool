6b94d87817ef5c7c5a6f25508de8054f29291be6
boobalaki <boobalakiruthra@gmail.com>
2024-04-26 16:20:07 +0530

    first commit

---
+++ nschool/src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import Ios_training from "./components/Ios";
+import FullStackComponent from "./components/fullstack";
+import ReactComponent from "./components/react";
+import AngularComponent from "./components/angular";
+import NodeJsComponent from "./components/nodejs";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <FullStackComponent />
+        {/* <ReactComponent /> */}
+        {/* <AngularComponent /> */}
+        {/* <NodeJsComponent /> */}
+
+{/* <Ios_training />    */}
+       
+      </div>
+    </div>
+  );
+}
+
+export default App;
